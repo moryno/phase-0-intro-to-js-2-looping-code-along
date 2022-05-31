863eb5f1fb0555e36e819ff1f70843eaf072bd3d
Maurice <mauricenganga41@gmail.com>
2022-05-31 17:24:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let messages = [];
+const writeCards = (names, eventName) => {
+    names.forEach((name) => {
+        messages.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`); 
+    })
+    return messages;
+}
+
+
+
+// for(let i = 0;i < names.length; i++){
+//     messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    
+// }
+
+const countDown = () => {
+    let count = 10;
+    while(count > -1){
+        console.log(count--)
+    }
+}
